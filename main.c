cff1895a40b390c57f1002d07e87b2228ac863a4
ki11errabbit <unlikelytitan@gmail.com>
2022-06-05 17:56:45 -0600

    gets line and separates each token into names and grades

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 typedef struct {
-    char firstName[30];
-    char lastName[30];
+    char* firstName;
+    char* lastName;
     int* grades[];
 } studentData;
 
@@ -33,18 +33,35 @@ int main(int argc, char** argv) {
         *studentList[i].grades = calloc(numGrades, sizeof(int));
     }
     int numReads = 0;
-    while (numReads < numStudents) {//fgets(readFile, 255, buff)) {
-        fscanf(readFile, "%[^\n]", buff );
-        //strncpy(studentList[numReads].firstName, buff, strcspn(buff, " ") );
-        *studentList[numReads].firstName = strtok(buff, " ");
-        //strncpy(studentList[numReads].lastName, buff, strcspn(buff, " ") );
-        *studentList[numReads].lastName = strtok(buff, " ");
-        for (int i = 0; i < numGrades; i++) {
-            studentList[numReads].grades[i] = atoi(strtok(buff, " "));
-            printf("%d\n", studentList[numReads].grades[i]);
+    char* newBuff;
+    size_t newBuffSize = 50;
+    while (getline(&newBuff, &newBuffSize, readFile)) {//fgets(readFile, 255, buff)) {
+
+        if (newBuff[0] != '\n') {
+            char* token = strtok(newBuff, " ");
+            studentList[numReads].firstName = calloc(sizeof(*token),sizeof(char));
+            strcpy(studentList[numReads].firstName, token);
+            printf("%s\n", studentList[numReads].firstName);
+            token = strtok(NULL, " ");
+
+            studentList[numReads].lastName = calloc(sizeof(*token),sizeof(char));
+            strcpy(studentList[numReads].lastName, token);
+            printf("%s\n", studentList[numReads].lastName);
+            token = strtok(NULL, " ");
+            int count = 0;
+            while (token != NULL) {
+                studentList[numReads].grades[count] = atoi(token);
+                printf(" %s\n", token);
+                token = strtok(NULL, " ");
+            }
+            //printf("%s\n",newBuff);
+
+
         }
-        printf("\n");
+
+
         numReads++;
+        if (numReads == numStudents +1) break;
     }
 
 
@@ -52,6 +69,8 @@ int main(int argc, char** argv) {
     printf("Hello, World!\n");
 
     for (int i = 0; i < numStudents; i++) {
+        //free(studentList[i].firstName);
+        //free(studentList[i].lastName);
         free(*studentList[i].grades);
     }
 
